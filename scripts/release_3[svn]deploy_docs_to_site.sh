67a4e95e88e392247e659a73f92c87019f352789
Nikolay Izhikov <nizhikov@apache.org>
2018-12-04 01:05:10 +0300

    Fix deploy docs script

--- scripts/release_3[svn]deploy_docs_to_site.sh
+++ scripts/release_3[svn]deploy_docs_to_site.sh
@@ -7,15 +7,15 @@ echo "Releasing ${ignite_version}${rc_name}"
 
 rm -r fabric
 
-unzip svn/vote/apache-ignite-fabric-*-bin.zip -d fabric
+unzip svn/vote/apache-ignite-*-bin.zip -d fabric
 
 echo "Uploading to svn ..."
 
 #uncomment subsequent line in case you want to remove incorrectly released vote
 #svn rm -m "Removing redundant Release" https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version || true
-svn import fabric/apache-ignite-fabric-$ignite_version-bin/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version -m "new Release ($ignite_version)"
-svn import fabric/apache-ignite-fabric-$ignite_version-bin/platforms/cpp/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version/cppdoc -m "new Release (${ignite_version})"
-svn import fabric/apache-ignite-fabric-$ignite_version-bin/platforms/dotnet/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version/dotnetdoc -m "new Release (${ignite_version})"
+svn import fabric/apache-ignite-$ignite_version-bin/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version -m "new Release ($ignite_version)"
+svn import fabric/apache-ignite-$ignite_version-bin/platforms/cpp/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version/cppdoc -m "new Release (${ignite_version})"
+svn import fabric/apache-ignite-$ignite_version-bin/platforms/dotnet/docs https://svn.apache.org/repos/asf/ignite/site/trunk/releases/$ignite_version/dotnetdoc -m "new Release (${ignite_version})"
 
 echo " "
 echo "======================================================"
